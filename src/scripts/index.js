e1b7fd1b9040b58d01544600aeaa44c4037b24c2
sushant14320 <sushant14320@gmail.com>
2020-04-15 20:13:02 +0530

    committed

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,146 @@
-<!-- Enter your JS code here -->
+
+
+document.onreadystatechange = function() { 
+  if (document.readyState !== "complete") { 
+      document.querySelector( 
+        "body").style.visibility = "hidden"; 
+      document.querySelector( 
+        "#loader").style.visibility = "visible"; 
+  } else { 
+      document.querySelector( 
+        "#loader").style.display = "none"; 
+      document.querySelector( 
+        "body").style.visibility = "visible"; 
+  } 
+};
+
+
+  getdata();
+
+  function search(){
+
+    
+var searchData = document.getElementById("search").value;
+const key = "897c2e65625e425396495470aef7fadd";
+
+const url = `https://newsapi.org/v2/everything?q=${searchData}&apiKey=${key}`
+
+UserSearch();
+async function UserSearch(){
+  const response  = await fetch(url);
+  const dataOf = await response.json();
+  console.log(dataOf);
+  if(dataOf.articles.length == 0)
+  {
+
+  
+    var nodata = ` <div class="not-found">No article was found based on the search.</div>`;
+    document.getElementById("news-articles").innerHTML = nodata;
+    console.log("else executed");
+  
+    
+} // end of then               
+else
+{
+  dataOf.articles.forEach(article =>{
+
+    const title = article.title;
+    const img = article.urlToImage;
+    const description = article.description;
+    const auth = article.author;
+    const link = article.url;
+
+    squery+=`
+    <li>
+    <div class="card" style="width:400px;">
+      <article class="card-body">
+            <img class="article-img card-img-top" src="${img}" height="300px" width="350px">
+            
+            <h2 class="article-title card-title">${title}</h2><br>
+            
+            <p class="article-description card-text">${description}</p><br>
+            
+            <span class="article-author"><em>${auth}</em></span><br>
+            
+            <a class="article-link" href="${link}"><b>Full article</b></a><br><br><br><br>
+    
+      </article>
+    </div>
+    </li>
+    
+    `
+    document.getElementById("news-articles").innerHTML = squery;
+
+  })
+  var squery =+ ''
+  
+  
+}
+
+
+
+} //end of for loop
+
+
+
+  }
+
+ 
+
+//Toggling the dark mode
+  function myFunction() {
+    var element = document.body;
+    
+    element.classList.toggle("dark-mode");
+   
+    
+ }
+
+ //fetching the data
+
+async function getdata() {
+  const res = await fetch('https://newsapi.org/v2/top-headlines?country=in&apiKey=897c2e65625e425396495470aef7fadd');
+  const data = await res.json();
+  
+  console.log(data);
+  
+ 
+  data.articles.forEach(news =>{
+    
+    const title = news.title;
+    const img = news.urlToImage;
+    const description = news.description;
+    const auth = news.author;
+    const link = news.url;
+
+
+
+    out+=`
+    <li>
+    <div class="card" style="width:400px;">
+      <article class="card-body">
+            <img class="article-img card-img-top" src="${img}" height="300px" width="350px">
+            
+            <h2 class="article-title card-title">${title}</h2><br>
+            
+            <p class="article-description card-text">${description}</p><br>
+            
+            <span class="article-author"><em>${auth}</em></span><br>
+            
+            <a class="article-link" href="${link}"><b>Full article</b></a><br><br><br><br>
+    
+      </article>
+    </div>
+    </li>
+    
+    `
+    document.getElementById("news-articles").innerHTML = out;
+        
+  })
+ var out =+ '';
+
+
+
+
+}
+
